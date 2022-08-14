41a1f660493936053de218ce9884f257e1d20ad9
ericssontee <ericssontee.eb@gmail.com>
2022-08-14 11:11:49 +0800

    Added Register Page

--- frontend/src/components/LoginPage.vue
+++ frontend/src/components/LoginPage.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="window-height window-width row justify-center items-center">
-    <q-card class="my-card" flat >
+    <q-card class="my-card" flat>
       <q-card-section horizontal>
         <q-card-section>
           <q-img
-          src="https://images.unsplash.com/photo-1472457897821-70d3819a0e24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80"
-          width="450px"
-          height="500px"
-          />
+            src="https://images.unsplash.com/photo-1472457897821-70d3819a0e24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80"
+            width="450px" height="500px" />
         </q-card-section>
         <q-card-section class="col-7 ">
           <q-input class="q-pb-sm" square outlined v-model="emailText" type="email" label="Email" />
           <q-input square outlined v-model="passText" type="password" label="Password" />
           <br />
           <q-btn @click="login(emailText, passText)">Login</q-btn>
+          <br />
+          <q-card-section horizontal>
+            <q-card-section>
+              <p>Not Registered Yet?</p>
+            </q-card-section>
+            <q-card-section>
+              <router-link v-bind:to="'/register'">Register</router-link>
+            </q-card-section>
+          </q-card-section>
         </q-card-section>
       </q-card-section>
     </q-card>
--- frontend/src/components/RegisterUserPage.vue
+++ frontend/src/components/RegisterUserPage.vue
@@ -1,3 +1,13 @@
 <template>
-<h1>Register Page</h1>
+    <div class="window-height window-width row justify-center items-center">
+        <q-card-section class="col-7 ">
+            <q-input v-model="nameText" label="Name" />
+            <q-input v-model="emailText" type="email" label="Email" />
+            <q-input v-model="passText" type="password" label="Password" />
+            <q-input v-model="conPassText" type="password" label="Confirm Password" />
+            <br />
+            <q-btn>Register</q-btn>
+            <q-btn :to="'/'">Cancel</q-btn>
+        </q-card-section>
+    </div>
 </template>
